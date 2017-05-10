06e04a94179de3ac97951eae4a05d1278871cce8
harry2636 <harry2636@naver.com>
2017-05-10 22:04:33 +0900

    Implemented search page UI.

---
+++ public/js/search.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('select.dropdown')
+	  .dropdown({
+	  	allowCategorySelection: true
+	  });
+
+$('.ui.dropdown')
+  .dropdown({
+  	allowCategorySelection: true
+  });
+});
