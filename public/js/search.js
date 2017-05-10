47e483d9b81a2ed5cd02d7643dcac5de7e2c0825
harry2636 <harry2636@naver.com>
2017-05-10 22:36:51 +0900

    Add dummy result for search.

--- public/js/search.js
+++ public/js/search.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
-	$('select.dropdown')
+	$(".ui.dropdown")
 	  .dropdown({
 	  	allowCategorySelection: true
 	  });
+	$("#search-button").on("click", function() {
+		this.html("");
+		this.html(`
+			<div class="row">
+				<div class="ui center aligned grid">
+					<div class="sixteen wide column">
+						<h3>1 buskers found</h3>
+					</div>
+				</div>
+			</div>
 
-$('.ui.dropdown')
-  .dropdown({
-  	allowCategorySelection: true
-  });
+			<div class="row">
+				<div class="ui sixteen wide column grid">
+					<div class="eight wide column">
+						<img class="ui image" id="search_example1" src="img/search_example1.jpg"/>
+					</div>
+					<div class="eight wide column">
+						<div class="row">
+							Name: Aaancod
+						</div>
+						<div class="row">
+							Genre: R&B
+						</div>
+						<div class="row">
+							Location: Hongdea, Shinchon, Gangnam
+						</div>
+						<div class="row">
+							Hashtags: #subway #guitar
+						</div>
+					</div>
+				</div>
+			</div>
+			`)
+	}.bind($("#search-result")));
+
+
 });
