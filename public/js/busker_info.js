99ba9e924e343e12e8927a3b82deaeb926269e33
kyungjejo <kyungjejo@gmail.com>
2017-05-20 00:41:33 +0900

    busker_info without feed done

---
+++ public/js/busker_info.js
@@ -0,0 +1,110 @@
+$("#hongdae1").click(function () {
+      $("iframe.map").attr(
+        {
+          "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=HongdaeStation",
+          "width":"400px",
+          "height":"400px"
+        });
+    });
+$("#hongdae2").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=HongdaeStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#hongdae3").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=HongdaeStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#sinchon1").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=SinchonStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#sinchon2").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=SinchonStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#gangnam").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=GangnamStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#daejeon").click(function () {
+  $("iframe.map").attr(
+    {
+      "src":"https://www.google.com/maps/embed/v1/place?key=AIzaSyCzHXjfvCCkYIyrEQPvdVOudynW0T_luYY&q=DaejeonStation",
+      "width":"400px",
+      "height":"400px"
+    });
+});
+$("#11").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/mH63DXpHQ70?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#21").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/mH63DXpHQ70?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#11").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/mH63DXpHQ70?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#12").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/M8vgPhi4vPk?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#22").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/M8vgPhi4vPk?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#13").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/H5QvzsCdvts?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#23").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/H5QvzsCdvts?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#14").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/3KSt2MgRLHU?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
+$("#24").click(function () {
+  $("iframe.video").attr({
+    "src":"https://www.youtube.com/embed/3KSt2MgRLHU?html5=1",
+    "width":"600px",
+    "height":"600px"
+  })});
