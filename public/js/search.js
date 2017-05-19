731f17c0c40ff2dc9247670e60a1bf820447950f
harry2636 <harry2636@naver.com>
2017-05-19 19:52:51 +0900

    Change design of search page.

--- public/js/search.js
+++ public/js/search.js
@@ -1,50 +1,78 @@
 $(document).ready(function() {
-	$(".ui.dropdown")
-	  .dropdown({
-	  	allowCategorySelection: true
+	$(".ui.dropdown").dropdown({
+          allowCategorySelection: true
 	  });
+    $(".ui.dropdown").dropdown('set selected', 'All')
 	$("#search-button").on("click", function() {
+        $("#search-result").text("1 result is found for the keyword");
 		this.html("");
-		this.html(`
-			<div class="row">
-				<div class="ui center aligned grid">
-					<div class="sixteen wide column">
-						<h3>1 buskers found</h3>
-					</div>
-				</div>
-			</div>
-
-			<div id="busker0" class="row">
-				<div class="ui sixteen wide column grid">
-					<div class="eight wide column">
-						<a href = "busker_info.html">
-							<img class="ui image" id="search_example1" src="img/BuskerHu.png" style="width: 600px; height: 400px;" />
-						</a>
-					</div>
-					<div class="eight wide column">
-						<div class="row">
-							Name: Seong Jung Hu
-						</div>
-						<div class="row">
-							Genre: R&B
-						</div>
-						<div class="row">
-							Location: Hongdea, Shinchon, Gangnam
-						</div>
-						<div class="row">
-							Hashtags: #subway #guitar
-						</div>
-					</div>
-				</div>
+		this.append(`
+            <div id="busker0" class="ui message search">
+                <div class="row">
+                    <div class="ui sixteen wide column grid">
+                        <div class="six wide column">
+                            <a href = "busker_info.html">
+                                <img class="ui image centered" id="search_example1" src="img/BuskerHu.png" style="height: 250px;" />
+                            </a>
+                        </div>
+                        <div class="ten wide column">
+                            <div class="row">
+                                Name: Seong Jung Hu
+                            </div>
+                            <div class="row">
+                                Genre: R&B
+                            </div>
+                            <div class="row">
+                                Location: Hongdea, Sinchon, Gangnam
+                            </div>
+                            <div class="row">
+                                Hashtags: #subway #guitar
+                            </div>
+                        </div>
+                    </div>
+                </div>
 			</div>
 			`)
-	}.bind($("#search-result")));
+        $("#busker0").css("cursor", "pointer");
+        $("#busker0").on("click", function() {
+            console.log("hi");
+            window.location.href = "./busker_info.html";
+            });
 
-	console.log("hi");
-	$("#busker0").on("click", function() {
-		console.log("hi");
-		window.location.href = "/busker_info.html";
-	});
 
+        this.append(`
+            <div id="busker0" class="ui message search">
+                <div class="row">
+                    <div class="ui sixteen wide column grid">
+                        <div class="six wide column">
+                            <a href = "busker_info.html">
+                                <img class="ui image centered" id="search_example1" src="img/BuskerHu.png" style="height: 250px;" />
+                            </a>
+                        </div>
+                        <div class="ten wide column">
+                            <div class="row">
+                                Name: Seong Jung Hu
+                            </div>
+                            <div class="row">
+                                Genre: R&B
+                            </div>
+                            <div class="row">
+                                Location: Hongdea, Sinchon, Gangnam
+                            </div>
+                            <div class="row">
+                                Hashtags: #subway #guitar
+                            </div>
+                        </div>
+                    </div>
+                </div>
+			</div>
+			`)
+        $("#busker0").css("cursor", "pointer");
+        $("#busker0").on("click", function() {
+            console.log("hi");
+            window.location.href = "./busker_info.html";
+        });
+
+    }.bind($("#search-content")));
 
 });
